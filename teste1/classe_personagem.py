36df5c241495eff2b08c119c0e7df2935f1e8c6a
sequeloble <andrephelipe@outlook.com>
2017-10-14 18:50:47 -0300

    modifica atributos

---
+++ teste1/classe_personagem.py
@@ -0,0 +1,77 @@
+from teste1.atributos_personagem import *
+
+class Cacador(AtributosPersonagem):
+    def __init__(self):
+        super().__init__(self, self, self, self, self, self, self, self)
+        self.forca = 1
+        self.constituicao = 1
+        self.destreza = 2
+        self.agilidade = 1
+        self.inteligencia = 1
+        self.forca_de_vontade = 1
+        self.percepcao = 2
+        self.carisma = 1
+        self.hp = AtributosPersonagem.pontos_de_vida(self, 10)
+        self.mp = AtributosPersonagem.pontos_de_magia(self, 5)
+
+
+class Campeao(AtributosPersonagem):
+    def __init__(self):
+        super().__init__(self, self, self, self, self, self, self, self)
+        self.forca = 2
+        self.constituicao = 2
+        self.destreza = 1
+        self.agilidade = 1
+        self.inteligencia = 1
+        self.forca_de_vontade = 1
+        self.percepcao = 1
+        self.carisma = 1
+        self.hp = AtributosPersonagem.pontos_de_vida(self, 12)
+        self.mp = AtributosPersonagem.pontos_de_magia(self, 5)
+
+
+class Guerreiro(AtributosPersonagem):
+    def __init__(self):
+        super().__init__(self, self, self, self, self, self, self, self)
+        self.forca = 2
+        self.constituicao = 1
+        self.destreza = 1
+        self.agilidade = 1
+        self.inteligencia = 1
+        self.forca_de_vontade = 2
+        self.percepcao = 1
+        self.carisma = 1
+        self.hp = AtributosPersonagem.pontos_de_vida(self, 12)
+        self.mp = AtributosPersonagem.pontos_de_magia(self, 5)
+
+
+
+class Ladino(AtributosPersonagem):
+    def __init__(self):
+        super().__init__(self, self, self, self, self, self, self, self)
+        self.forca = 1
+        self.constituicao = 1
+        self.destreza = 2
+        self.agilidade = 2
+        self.inteligencia = 1
+        self.forca_de_vontade = 1
+        self.percepcao = 1
+        self.carisma = 1
+        self.hp = AtributosPersonagem.pontos_de_vida(self, 8)
+        self.mp = AtributosPersonagem.pontos_de_magia(self, 5)
+
+
+
+class Mago(AtributosPersonagem):
+    def __init__(self):
+        super().__init__(self, self, self, self, self, self, self, self)
+        self.forca = 1
+        self.constituicao = 1
+        self.destreza = 1
+        self.agilidade = 1
+        self.inteligencia = 2
+        self.forca_de_vontade = 2
+        self.percepcao = 1
+        self.carisma = 1
+        self.hp = AtributosPersonagem.pontos_de_vida(self, 8)
+        self.mp = AtributosPersonagem.pontos_de_magia(self, 10)
