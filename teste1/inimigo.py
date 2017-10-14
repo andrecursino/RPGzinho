db082dcab58c560c4429c9122128ba95818ab35b
sequeloble <andrephelipe@outlook.com>
2017-10-14 18:55:37 -0300

    classe inimigo

---
+++ teste1/inimigo.py
@@ -0,0 +1,15 @@
+from teste1.atributos_personagem import *
+
+class Inimigo(AtributosPersonagem):
+    def __init__(self):
+        super().__init__(self, self, self, self, self, self, self, self)
+        self.forca = 1
+        self.constituicao = 1
+        self.destreza = 1
+        self.agilidade = 1
+        self.inteligencia = 1
+        self.forca_de_vontade = 1
+        self.percepcao = 1
+        self.carisma = 1
+        self.hp = AtributosPersonagem.pontos_de_vida(self, 6)
+        self.mp = AtributosPersonagem.pontos_de_magia(self, 3)
