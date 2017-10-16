7d5fef6970bf54d5e95a6e6c182a47b63059a243
sequeloble <andrephelipe@outlook.com>
2017-10-16 06:46:45 -0200

    main

--- teste1/main.py
+++ teste1/main.py
@@ -1,7 +1,9 @@
 from teste1.classe_personagem import *
 from teste1.inimigo import *
+from teste1.escolhe_classe import *
+from teste1.combate import *
 
-jao = Cacador()
-ini = Inimigo()
-print(ini.hp)
-print(jao.mp)
+heroi = escolhe_classe()
+inimigo = Inimigo()
+
+while heroi.hp > 0 or inimigo.hp > 0:
